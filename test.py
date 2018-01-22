362114411e013b6536c59d7ccc2fe3eb57a7ae9f
Patryk Konior <patryk@arteia.co>
2018-01-22 20:40:07 +0100

    loading from file

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import logging as log
 import datetime as dt
 from time import sleep
 
+fromFile = sys.argv[1]
+
+videoPath = "videoplayback.mp4"
 faceCascPath = "haarcascade_frontalface_default.xml"
 smileCascPath = "haarcascade_smile.xml"
 eyeCascPath = "haarcascade_eye.xml"
@@ -12,54 +15,90 @@ faceCascade = cv2.CascadeClassifier(faceCascPath)
 # smileCascade = cv2.CascadeClassifier(smileCascPath)
 eyeCascade = cv2.CascadeClassifier(eyeCascPath)
 
-log.basicConfig(filename='webcam.log',level=log.INFO)
+if fromFile == 'fromFile':
+    video_capture = cv2.VideoCapture(videoPath)
 
-video_capture = cv2.VideoCapture(0)
-anterior = 0
+    while(video_capture.isOpened()):
+        ret, frame = video_capture.read()
 
-while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+        ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            
+            eyes = eyeCascade.detectMultiScale(roi_gray)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        
-        eyes = eyeCascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
 
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+    cap.release()
+    cv2.destroyAllWindows()
+else:
+    log.basicConfig(filename='webcam.log',level=log.INFO)
 
+    video_capture = cv2.VideoCapture(0)
+    anterior = 0
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+    while True:
+        if not video_capture.isOpened():
+            print('Unable to load camera.')
+            sleep(5)
+            pass
 
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            
+            eyes = eyeCascade.detectMultiScale(roi_gray)
+
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+
+        if anterior != len(faces):
+            anterior = len(faces)
+            log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
