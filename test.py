04d80abfcbd9167330e257d0871c93dbf5b9cfa7
Patryk Konior <patryk@arteia.co>
2018-01-22 20:23:41 +0100

    recognizing eyes on face works

--- test.py
+++ test.py
@@ -1,22 +1,65 @@
-import numpy as np
 import cv2
+import sys
+import logging as log
+import datetime as dt
+from time import sleep
 
+faceCascPath = "haarcascade_frontalface_default.xml"
+smileCascPath = "haarcascade_smile.xml"
+eyeCascPath = "haarcascade_eye.xml"
 
+faceCascade = cv2.CascadeClassifier(faceCascPath)
+# smileCascade = cv2.CascadeClassifier(smileCascPath)
+eyeCascade = cv2.CascadeClassifier(eyeCascPath)
 
-cap = cv2.VideoCapture(0)
+log.basicConfig(filename='webcam.log',level=log.INFO)
 
-while(True):
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = video_capture.read()
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        eyes = eyeCascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('Video', frame)
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
-cap.release()
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
