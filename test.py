aaad8bc7d5168b9db6090bcc81d54128ce50237b
wgrabis <wgrabis@gmail.com>
2018-01-22 12:43:37 -0800

    Eye blur

--- test.py
+++ test.py
@@ -18,15 +18,15 @@ eyeCascade = cv2.CascadeClassifier(eyeCascPath)
 if fromFile == 'fromFile':
     video_capture = cv2.VideoCapture(videoPath)
 
-    while(video_capture.isOpened()):
+    while (video_capture.isOpened()):
         ret, frame = video_capture.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('frame',gray)
+        cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
+
         ret, frame = video_capture.read()
 
         faces = faceCascade.detectMultiScale(
@@ -38,21 +38,39 @@ if fromFile == 'fromFile':
 
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+
             eyes = eyeCascade.detectMultiScale(roi_gray)
 
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+            for (ex, ey, ew, eh) in eyes:
+                newx = ex + x
+                newy = ey + y
+
+
+                # crop_out = cv2.rectangle(ex, ey, ew, eh)
+
+                print ex, ey, ew, eh
+
+                roi_eye = frame[y:newy + eh, x:newx + ew]
+
+                print roi_eye
+
+                roi_eye = cv2.GaussianBlur(roi_eye, (23, 23), 30)
+
+                print roi_eye
+
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
+                frame[y:newy + eh, x:newx + ex] = roi_eye
+
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
     video_capture.release()
     cv2.destroyAllWindows()
 else:
-    log.basicConfig(filename='webcam.log',level=log.INFO)
+    log.basicConfig(filename='webcam.log', level=log.INFO)
 
     video_capture = cv2.VideoCapture(0)
     anterior = 0
@@ -77,18 +95,18 @@ else:
 
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = frame[y:y + h, x:x + w]
+
             eyes = eyeCascade.detectMultiScale(roi_gray)
 
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
 
         if anterior != len(faces):
             anterior = len(faces)
-            log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+            log.info("faces: " + str(len(faces)) + " at " + str(dt.datetime.now()))
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
